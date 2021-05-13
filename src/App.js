3b490ee210ed409634230dcdd6666fc78c5fe325
AndreiAnisia <anisia_andrei_88@yahoo.com>
2021-05-13 17:44:19 +0300

    1st part of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const alanKey =
+   '43c3a25714e0ec14cb772ad893afc26a2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+   const [newsArticles, setNewsArticles] = useState([]);
+   const classes = useStyles();
+
+   useEffect(() => {
+      alanBtn({
+         key: alanKey,
+         onCommand: ({ command, articles }) => {
+            if (command === 'newHeadlines') {
+               console.log(articles);
+               setNewsArticles(articles);
+            }
+         },
+      });
+   }, []);
+
+   return (
+      <div>
+         <div className={classes.logoContainer}>
+            <img
+               className={classes.alanLogo}
+               alt="alan logo"
+               src="https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg"
+            />
+         </div>
+         <NewsCards articles={newsArticles} />
+      </div>
+   );
+};
 
 export default App;
